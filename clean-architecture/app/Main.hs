0625f1b3e8dba911c3db4517721ae3e72201c4c4
竹内 智昭 <takeuchi-tomoaki@dmm.com>
2020-03-03 11:50:42 +0900

    main修正

--- clean-architecture/app/Main.hs
+++ clean-architecture/app/Main.hs
@@ -2,10 +2,28 @@ module Main where
 
 import Controllers.Employee.EmployeeController(callCreateEmployee)
 
+typeGet = 1 :: Int
+typeCreate = 2 :: Int
+typeUpdate = 3 :: Int
+typeDelete = 4 :: Int
+
 main :: IO ()
 main = do
-    createMethod
+    putStrLn "plese select number"
+    putStrLn ("if get data then " ++ show typeGet)
+    putStrLn ("and if make data then " ++ show typeCreate)
+    putStrLn ("and if update data then " ++ show typeUpdate)
+    putStrLn ("and if delete data then " ++ show typeDelete)
+    methodType <- getLine
 
+    excute $ read methodType
+
+excute :: Int -> IO ()
+excute methodType
+    | methodType == typeGet    = getMethod
+    | methodType == typeCreate = createMethod
+    | methodType == typeUpdate = updateMethod
+    | methodType == typeDelete = deleteMethod
 
 createMethod :: IO ()
 createMethod = do
@@ -15,3 +33,15 @@ createMethod = do
     age <- getLine
 
     callCreateEmployee name age
+
+getMethod :: IO ()
+getMethod = do
+    putStrLn "GET!"
+
+updateMethod :: IO ()
+updateMethod = do
+    putStrLn "UPDATE!"
+
+deleteMethod :: IO ()
+deleteMethod = do
+    putStrLn "DELETE!"
