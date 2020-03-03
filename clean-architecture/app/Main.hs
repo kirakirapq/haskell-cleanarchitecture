67417e5efd1e491ba76d8b028455082f7749253c
竹内 智昭 <takeuchi-tomoaki@dmm.com>
2020-03-03 11:13:59 +0900

    メソッド化

--- clean-architecture/app/Main.hs
+++ clean-architecture/app/Main.hs
@@ -4,6 +4,11 @@ import Controllers.Employee.EmployeeController(callCreateEmployee)
 
 main :: IO ()
 main = do
+    createMethod
+
+
+createMethod :: IO ()
+createMethod = do
     putStrLn "what`s your  name?"
     name <- getLine
     putStrLn "how old are you?"
